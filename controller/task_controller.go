44405b35a6503b71ea85e9ba31527b852a34e556
mocha-06 <mocha.isa.0625@gmail.com>
2023-10-14 02:24:34 +0900

    first commit

---
+++ controller/task_controller.go
@@ -0,0 +1,48 @@
+package controller
+
+import (
+	"crypto/ecdh"
+	"go-rest-api/usecase"
+	"net/http"
+	"strconv"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/labstack/echo/v4"
+)
+
+type ITaskController interface {
+	GetAllTasks(c echo.Context) error
+	GetTaskById(c echo.Context) error
+	CreateTask(c echo.Context) error
+	UpdateTask(c echo.Context) error
+	DeleteTask(c echo.Context) error
+}
+
+type taskController struct {
+	tu usecase.ITaskUsecase
+}
+
+func NewTaskController(tu usecase.ITaskUsecase) ITaskController {
+	return &taskController{tu}
+}
+
+func (tc *taskController) GetAllTasks(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	userId := claims["user_id"]
+
+	tasksRes, err := tc.tu.GetAllTasks(uint(userId.(float64)))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusOK, tasksRes)
+}
+
+func (tc *taskController) GetTaskById(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	userId :=claims["user_id"]
+	id := c.Param("taskId")
+	taskId, _ :=strconv.Atoi(id)
+	
+}
