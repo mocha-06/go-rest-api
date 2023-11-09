dbf9546111c700e7633f28d37383e243c510954e
mocha-06 <mocha.isa.0625@gmail.com>
2023-11-09 12:43:53 +0900

    📝 add comment

--- usecase/task_usecase.go
+++ usecase/task_usecase.go
@@ -6,6 +6,11 @@ import (
 	"go-rest-api/validator"
 )
 
+// taskに関する操作（取得、作成、更新、削除）を実装
+// コントローラからのリクエストに応じたデータベース操作を実行
+// バリデーションとエラーハンドリングも行う
+
+// taskの操作を定義
 type ITaskUsecase interface {
 	GetAllTasks(userId uint) ([]model.TaskResponse, error)
 	GetTaskById(userId uint, taskId uint) (model.TaskResponse, error)
@@ -14,21 +19,28 @@ type ITaskUsecase interface {
 	DeleteTask(userId uint, taskId uint) error
 }
 
+// 構造体にインターフェースを実装
 type taskUsecase struct {
 	tr repository.ITaskRepository
 	tv validator.ITaskValidator
 }
 
+// taskUsecaseインスタンスを作成
 func NewTaskUsecase(tr repository.ITaskRepository, tv validator.ITaskValidator) ITaskUsecase {
 	return &taskUsecase{tr, tv}
 }
 
+// 取得タスクは model.TaskResponse に変換され、返却
 func (tu *taskUsecase) GetAllTasks(userId uint) ([]model.TaskResponse, error) {
 	tasks := []model.Task{}
+	// taskRepositoryのITaskRepositoryに定義されているfunction
+	// 取得できたタスクはtasksスライスに格納、errはnil 失敗するとerr
 	if err := tu.tr.GetAllTasks(&tasks, userId); err != nil {
 		return nil, err
 	}
+	// 返却データを格納するためのスライスを実装
 	resTasks := []model.TaskResponse{}
+	// resTasksに対して、tasksのデータをtのフォーマットとして追加していく
 	for _, v := range tasks {
 		t := model.TaskResponse{
 			ID:        v.ID,
@@ -55,6 +67,7 @@ func (tu *taskUsecase) GetTaskById(userId uint, taskId uint) (model.TaskResponse
 	return resTask, nil
 }
 
+// タスクが作成され、 model.TaskResponse に変換されて返却
 func (tu *taskUsecase) CreateTask(task model.Task) (model.TaskResponse, error) {
 	if err := tu.tv.TaskValidate(task); err != nil {
 		return model.TaskResponse{}, err
@@ -71,6 +84,7 @@ func (tu *taskUsecase) CreateTask(task model.Task) (model.TaskResponse, error) {
 	return resTask, nil
 }
 
+// タスクが更新され、 model.TaskResponse に変換されて返却
 func (tu *taskUsecase) UpdateTask(task model.Task, userId uint, taskId uint) (model.TaskResponse, error) {
 	if err := tu.tv.TaskValidate(task); err != nil {
 		return model.TaskResponse{}, err
@@ -88,6 +102,7 @@ func (tu *taskUsecase) UpdateTask(task model.Task, userId uint, taskId uint) (mo
 	return resTask, nil
 }
 
+// 削除が成功した場合はエラーnil、失敗した場合はエラーが返却
 func (tu *taskUsecase) DeleteTask(userId uint, taskId uint) error {
 	if err := tu.tr.DeleteTask(userId, taskId); err != nil {
 		return err
